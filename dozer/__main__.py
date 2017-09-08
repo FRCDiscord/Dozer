d6ee18858ec56166508ff055b424e03d555c7660
Ian W <ian.uu12@gmail.com>
2017-09-08 16:32:47 -0700

    Runnable bot with auto-generated config

--- dozer/__main__.py
+++ dozer/__main__.py
@@ -1,8 +1,22 @@
-import json
-from . import Dozer
+import json, os.path, sys
 
-with open('config.json') as f:
-	config = json.load(f)
-	bot = Dozer(config)
+config = {'prefix': '&'}
+config_file = 'config.json'
 
+if os.path.isfile(config_file):
+	with open(config_file) as f:
+		config.update(json.load(f))
+
+with open('config.json', 'w') as f:
+	json.dump(config, f, indent='\t')
+
+if 'discord_token' not in config:
+	sys.exit('Discord token must be supplied in configuration')
+
+if sys.version_info < (3, 5):
+	sys.exit('Dozer requires Python 3.5 or higher to run. This is version %s.' % '.'.join(sys.version_info[:3]))
+
+from . import Dozer # After version check
+
+bot = Dozer(config)
 bot.run()
