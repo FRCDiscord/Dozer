956afd3976e79f1f103034fa68f9d235ce7d00b3
Ian W <ian.uu12@gmail.com>
2017-09-11 04:48:11 -0700

    giveme list; s/givable/giveable (spelling)

--- dozer/cogs/roles.py
+++ dozer/cogs/roles.py
@@ -7,12 +7,12 @@ class Roles(Cog):
 	@group(invoke_without_command=True)
 	@bot_has_permissions(manage_roles=True)
 	async def giveme(self, ctx, *, roles):
-		"""Give you one or more givable roles, separated by commas."""
+		"""Give you one or more giveable roles, separated by commas."""
 		requests = set(name.strip().casefold() for name in roles.split(','))
-		givables = self.givable_roles(ctx.guild)
+		giveables = self.giveable_roles(ctx.guild)
 		
-		already_have = set(role for name, role in givables.items() if name in requests and role in ctx.author.roles)
-		valid = set(role for name, role in givables.items() if name in requests and role not in already_have)
+		already_have = set(role for name, role in giveables.items() if name in requests and role in ctx.author.roles)
+		valid = set(role for name, role in giveables.items() if name in requests and role not in already_have)
 		
 		await ctx.author.add_roles(*valid)
 		
@@ -23,60 +23,60 @@ class Roles(Cog):
 			e.add_field(name='You already have {} role(s)!'.format(len(already_have)), value='\n'.join(role.name for role in already_have), inline=False)
 		extra = len(requests) - (len(already_have) + len(valid))
 		if extra > 0:
-			e.add_field(name='{} role(s) could not be found!'.format(extra), value='Use `{0.prefix}{0.invoked_with} list` to find valid givable roles!'.format(ctx), inline=False)
+			e.add_field(name='{} role(s) could not be found!'.format(extra), value='Use `{0.prefix}{0.invoked_with} list` to find valid giveable roles!'.format(ctx), inline=False)
 		await ctx.send(embed=e)
 	
 	@giveme.command()
 	@bot_has_permissions(manage_roles=True)
 	@has_permissions(manage_guild=True)
 	async def add(self, ctx, *, name):
-		"""Makes an existing role givable, or creates one if it doesn't exist. Name must not contain commas.
+		"""Makes an existing role giveable, or creates one if it doesn't exist. Name must not contain commas.
 		Similar to create, but will use an existing role if one exists."""
 		if ',' in name:
-			raise BadArgument('givable role names must not contain commas!')
+			raise BadArgument('giveable role names must not contain commas!')
 		role = discord.utils.get(ctx.guild.roles, name=name)
 		if role is None:
-			role = await ctx.guild.create_role(name=name, reason='Givable role created by {}'.format(ctx.author))
-		elif name.strip().casefold() in self.givable_roles(ctx.guild):
-			raise BadArgument('that role already exists and is givable!')
+			role = await ctx.guild.create_role(name=name, reason='Giveable role created by {}'.format(ctx.author))
+		elif name.strip().casefold() in self.giveable_roles(ctx.guild):
+			raise BadArgument('that role already exists and is giveable!')
 		with db.Session() as session:
-			givables = session.query(GivableRoles).filter_by(guild_id=ctx.guild.id).first()
-			if givables is None:
-				givables = GivableRoles(guild_id=ctx.guild.id, role_ids=str(role.id))
-				session.add(givables)
+			giveables = session.query(GiveableRoles).filter_by(guild_id=ctx.guild.id).first()
+			if giveables is None:
+				giveables = GiveableRoles(guild_id=ctx.guild.id, role_ids=str(role.id))
+				session.add(giveables)
 			else:
-				givables.role_ids += ' ' + str(role.id)
-		await ctx.send('Added givable role {0.name!r}! Use `{1}{2} {0}` to get it!'.format(role, ctx.prefix, ctx.command.parent))
+				giveables.role_ids += ' ' + str(role.id)
+		await ctx.send('Added giveable role {0.name!r}! Use `{1}{2} {0}` to get it!'.format(role, ctx.prefix, ctx.command.parent))
 	
 	@giveme.command()
 	@bot_has_permissions(manage_roles=True)
 	@has_permissions(manage_guild=True)
 	async def create(self, ctx, *, name):
-		"""Create a givable role. Name must not contain commas.
+		"""Create a giveable role. Name must not contain commas.
 		Similar to add, but will always create a new role."""
 		if ',' in name:
-			raise BadArgument('givable role names must not contain commas!')
-		if name.strip().casefold() in self.givable_roles(ctx.guild):
-			raise BadArgument('a duplicate role is givable and would conflict!')
-		role = await ctx.guild.create_role(name=name, reason='Givable role created by {}'.format(ctx.author))
+			raise BadArgument('giveable role names must not contain commas!')
+		if name.strip().casefold() in self.giveable_roles(ctx.guild):
+			raise BadArgument('a duplicate role is giveable and would conflict!')
+		role = await ctx.guild.create_role(name=name, reason='Giveable role created by {}'.format(ctx.author))
 		with db.Session() as session:
-			givables = session.query(GivableRoles).filter_by(guild_id=ctx.guild.id).first()
-			if givables is None:
-				givables = GivableRoles(guild_id=ctx.guild.id, role_ids=str(role.id))
-				session.add(givables)
+			giveables = session.query(GiveableRoles).filter_by(guild_id=ctx.guild.id).first()
+			if giveables is None:
+				giveables = GiveableRoles(guild_id=ctx.guild.id, role_ids=str(role.id))
+				session.add(giveables)
 			else:
-				givables.role_ids += ' ' + str(role.id)
-		await ctx.send('Created givable role {0.name!r}! Use `{1}{2} {0}` to get it!'.format(role, ctx.prefix, ctx.command.parent))
+				giveables.role_ids += ' ' + str(role.id)
+		await ctx.send('Created giveable role {0.name!r}! Use `{1}{2} {0}` to get it!'.format(role, ctx.prefix, ctx.command.parent))
 	
 	@giveme.command()
 	@bot_has_permissions(manage_roles=True)
 	async def remove(self, ctx, *, roles):
-		"""Removes multiple givable roles from you. Names must be separated by commas."""
+		"""Removes multiple giveable roles from you. Names must be separated by commas."""
 		requests = set(name.strip().casefold() for name in roles.split(','))
-		givables = self.givable_roles(ctx.guild)
+		giveables = self.giveable_roles(ctx.guild)
 		
-		valid = set(role for name, role in givables.items() if name in requests and role in ctx.author.roles)
-		dont_have = set(role for name, role in givables.items() if name in requests and role not in valid)
+		valid = set(role for name, role in giveables.items() if name in requests and role in ctx.author.roles)
+		dont_have = set(role for name, role in giveables.items() if name in requests and role not in valid)
 		
 		await ctx.author.remove_roles(*valid)
 		
@@ -87,36 +87,44 @@ class Roles(Cog):
 			e.add_field(name='You didn\'t have {} role(s)!'.format(len(dont_have)), value='\n'.join(role.name for role in dont_have), inline=False)
 		extra = len(requests) - (len(valid) + len(dont_have))
 		if extra > 0:
-			e.add_field(name='{} role(s) could not be found!'.format(extra), value='Use `{0.prefix}{0.command.parent} list` to find valid givable roles!'.format(ctx), inline=False)
+			e.add_field(name='{} role(s) could not be found!'.format(extra), value='Use `{0.prefix}{0.command.parent} list` to find valid giveable roles!'.format(ctx), inline=False)
 		await ctx.send(embed=e)
 	
 	@giveme.command()
 	@bot_has_permissions(manage_roles=True)
 	@has_permissions(manage_guild=True)
 	async def delete(self, ctx, *, name):
-		"""Deletes and removes a givable role."""
-		roles = self.givable_roles(ctx.guild)
+		"""Deletes and removes a giveable role."""
+		roles = self.giveable_roles(ctx.guild)
 		stripped = name.casefold().strip()
 		if stripped not in roles:
-			raise BadArgument('{} is not a givable role!'.format(name))
+			raise BadArgument('{} is not a giveable role!'.format(name))
 		role = roles[stripped]
 		await role.delete()
 		with db.Session() as session:
-			givables = session.query(GivableRoles).filter_by(guild_id=ctx.guild.id).first() # Null-checked by givables containing name
-			givables.role_ids = givables.role_ids.replace(str(role), '').replace('  ', ' ').strip()
+			giveables = session.query(GiveableRoles).filter_by(guild_id=ctx.guild.id).first() # Null-checked by giveables containing name
+			giveables.role_ids = giveables.role_ids.replace(str(role.id), '').replace('  ', ' ').strip()
 		await ctx.send('Role {0.name!r} has been deleted!'.format(role))
 	
+	@giveme.command(name='list')
+	@bot_has_permissions(manage_roles=True)
+	async def list_roles(self, ctx):
+		roles = self.giveable_roles(ctx.guild)
+		e = discord.Embed(color=discord.Color.blue(), title='Giveable roles')
+		e.description = '\n'.join(role.name for role in roles.values())
+		await ctx.send(embed=e)
+	
 	@staticmethod
-	def givable_roles(guild):
+	def giveable_roles(guild):
 		with db.Session() as session:
-			roles = session.query(GivableRoles).filter_by(guild_id=guild.id).first()
+			roles = session.query(GiveableRoles).filter_by(guild_id=guild.id).first()
 		if roles is None:
 			return {}
-		givable_ids = [int(id_) for id_ in roles.role_ids.split(' ')]
-		return {role.name.strip().casefold(): role for role in guild.roles if role.id in givable_ids}
+		giveable_ids = [int(id_) for id_ in roles.role_ids.split(' ')]
+		return {role.name.strip().casefold(): role for role in guild.roles if role.id in giveable_ids}
 
-class GivableRoles(db.DatabaseObject):
-	__tablename__ = 'givable_roles'
+class GiveableRoles(db.DatabaseObject):
+	__tablename__ = 'giveable_roles'
 	guild_id = db.Column(db.Integer, primary_key=True)
 	role_ids = db.Column(db.String, nullable=True)
 
