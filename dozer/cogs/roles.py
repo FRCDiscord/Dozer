7532a693d47345b4b62456852b28439adc990528
Ian W <ian.uu12@gmail.com>
2017-09-11 03:43:04 -0700

    giveme add and giveme create; fix giveme

--- dozer/cogs/roles.py
+++ dozer/cogs/roles.py
@@ -1,12 +1,12 @@
 import discord
-from discord.ext.commands import bot_has_permissions
+from discord.ext.commands import bot_has_permissions, BadArgument
 from .. import db
 from ._utils import *
 
 class Roles(Cog):
 	@group(invoke_without_command=True)
 	@bot_has_permissions(manage_roles=True)
-	async def giveme(self, ctx):
+	async def giveme(self, ctx, *, roles):
 		"""Give you one or more givable roles, separated by commas."""
 		requests = set(name.strip().casefold() for name in roles.split(','))
 		givables = self.givable_roles(ctx.guild)
@@ -25,6 +25,46 @@ class Roles(Cog):
 		if extra > 0:
 			e.add_field(name='{} role(s) could not be found!'.format(extra), value='Use `{0.prefix}{0.invoked_with} list` to find valid givable roles!'.format(ctx), inline=False)
 		await ctx.send(embed=e)
+	
+	@giveme.command()
+	@bot_has_permissions(manage_roles=True)
+	async def add(self, ctx, *, name):
+		"""Makes an existing role givable, or creates one if it doesn't exist. Name must not contain commas.
+		Similar to create, but will use an existing role if one exists."""
+		if ',' in name:
+			raise BadArgument('givable role names must not contain commas!')
+		role = discord.utils.get(ctx.guild.roles, name=name)
+		if role is None:
+			role = await ctx.guild.create_role(name=name, reason='Givable role created by {}'.format(ctx.author))
+		elif name.strip().casefold() in self.givable_roles(ctx.guild):
+			raise BadArgument('that role already exists and is givable!')
+		with db.Session() as session:
+			givables = session.query(GivableRoles).filter_by(guild_id=ctx.guild.id).first()
+			if givables is None:
+				givables = GivableRoles(guild_id=ctx.guild.id, role_ids=str(role.id))
+				session.add(givables)
+			else:
+				givables.role_ids += ' ' + str(role.id)
+		await ctx.send('Created givable role {0}! Use `{1}{2} {0}` to get it!'.format(role, ctx.prefix, ctx.command.parent))
+	
+	@giveme.command()
+	@bot_has_permissions(manage_roles=True)
+	async def create(self, ctx, *, name):
+		"""Create a givable role. Name must not contain commas.
+		Similar to add, but will always create a new role."""
+		if ',' in name:
+			raise BadArgument('givable role names must not contain commas!')
+		if name.strip().casefold() in self.givable_roles(ctx.guild):
+			raise BadArgument('a duplicate role is givable and would conflict!')
+		role = await ctx.guild.create_role(name=name, reason='Givable role created by {}'.format(ctx.author))
+		with db.Session() as session:
+			givables = session.query(GivableRoles).filter_by(guild_id=ctx.guild.id).first()
+			if givables is None:
+				givables = GivableRoles(guild_id=ctx.guild.id, role_ids=str(role.id))
+				session.add(givables)
+			else:
+				givables.role_ids += ' ' + str(role.id)
+		await ctx.send('Created givable role {0}! Use `{1}{2} {0}` to get it!'.format(role, ctx.prefix, ctx.command.parent))
 	
 	@staticmethod
 	def givable_roles(guild):
