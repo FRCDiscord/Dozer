197f36307751249610f82634c8c4f869768754fa
Michael Cao <mike.x.cao@gmail.com>
2017-09-17 12:22:01 -0400

    Update moderation.py

--- dozer/cogs/moderation.py
+++ dozer/cogs/moderation.py
@@ -8,29 +8,22 @@ class Moderation(Cog):
         usertoban = user_mentions
         usertobanstr = str(usertoban)
         print("Ban detected for user", usertobanstr)
-        await ctx.send("Banning " + usertobanstr)
-        print(usertoban)
         await ctx.guild.ban(usertoban)
-        bannedid = str(user_mentions.id)
-        correctprefix = ctx.prefix
-        howtounban = "When it's time to unban, run " + correctprefix + "unban <@" + bannedid + " >"
-        await ctx.send(howtounban)
+        await ctx.send(usertobanstr + " has been banned.")
     @command()
     async def unban(self, ctx, user_mentions: discord.User):
         "Unbans the user ID mentioned."
         usertoban = user_mentions
         usertobanstr = str(usertoban)
-        print("Unban detected for user", usertobanstr)
-        await ctx.send("Unbanning " + usertobanstr)
+        print("Unban detected for user" + usertobanstr)
         print(usertoban)
         await ctx.guild.unban(usertoban)
+        await ctx.send(usertobanstr + "has been unbanned")
     @command()
     async def kick(self, ctx, user_mentions: discord.User):
         "Kicks the user mentioned."
         usertokick = user_mentions
-        usertokickstr = str(usertokick)
-        await ctx.send("Kicking " + usertokickstr)
-        print("Kick detected for " + usertokickstr)
         await ctx.guild.kick(usertokick)
+        await ctx.send(usertokickstr + " has been kicked")
 def setup(bot):
     bot.add_cog(Moderation(bot))
