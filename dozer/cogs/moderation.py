097d47bff90d964ab0fd05de53f63a59c2fc9b12
Travis Weir <twgamefreak@gmail.com>
2017-09-17 18:59:57 -0500

    Perms checks + getting the ID to ban

--- dozer/cogs/moderation.py
+++ dozer/cogs/moderation.py
@@ -1,16 +1,23 @@
+from discord.ext.commands import has_permissions
+
 from ._utils import *
 import discord
 
 class Moderation(Cog):
     @command()
+    @has_permissions(manage_guild=True)
     async def ban(self, ctx, user_mentions: discord.User):
         "Bans the user mentioned."
         usertoban = user_mentions
         usertobanstr = str(usertoban)
+        bannedid = str(usertoban.id)
         print("Ban detected for user", usertobanstr)
         await ctx.guild.ban(usertoban)
         await ctx.send(usertobanstr + " has been banned.")
+        howtounban = "When it's time to unban, here's the ID to unban: <@" + bannedid + " >"
+        await ctx.send(howtounban)
     @command()
+    @has_permissions(manage_guild=True)
     async def unban(self, ctx, user_mentions: discord.User):
         "Unbans the user ID mentioned."
         usertoban = user_mentions
@@ -20,10 +27,9 @@ class Moderation(Cog):
         await ctx.guild.unban(usertoban)
         await ctx.send(usertobanstr + "has been unbanned")
     @command()
+    @has_permissions(manage_guild=True)
     async def kick(self, ctx, user_mentions: discord.User):
         "Kicks the user mentioned."
-        haspermissions = ctx.channel.permissions_for(ctx.message.author)
-        print(haspermissions)
         usertokick = user_mentions
         usertokickstr = str(usertokick)
         await ctx.guild.kick(usertokick)
