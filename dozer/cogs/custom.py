1573b831fb998e3b9df8a441d73f6fb94e429cd4
770352 <770352@monashores.net>
2017-10-04 19:12:49 -0400

    Fixed IFs

--- dozer/cogs/custom.py
+++ dozer/cogs/custom.py
@@ -23,7 +23,7 @@ class Custom(Cog):
 	async def tba (self,ctx,task,teamnum):
 		"""Retrieve information about this guild."""
 		parser = tbapi.TBAParser('3572', 'Dozer', 'Alpha 0.1')
-		if task = team
+		if task == team:
 		team = parser.get_team('frc' + teamnum)
 		guild = ctx.guild
 		e = discord.Embed(color=blurple)
@@ -36,7 +36,7 @@ class Custom(Cog):
 		e.add_field(name='Team Website', value=team.website)
 		e.add_field(name='TBA Page', value='https://www.thebluealliance.com/team/' + teamnum)
 		await ctx.send(embed=e)
-		if task = awards
+		if task == awards:
 		await ctx.send('Awards' + teamnum)
 def setup(bot):
 	bot.add_cog(Custom(bot))
