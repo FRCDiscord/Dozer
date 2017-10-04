1b5b912d13dcf3fb610a438440851f72e2bae866
770352 <770352@monashores.net>
2017-10-04 19:05:56 -0400

    TBA Tasks?

--- dozer/cogs/custom.py
+++ dozer/cogs/custom.py
@@ -20,10 +20,11 @@ class Custom(Cog):
 		"""This is for checking if an instance of Dozer has this command."""
 		await ctx.send('YES. This Dozer has custom!')
 	@command()
-	async def tba (self,ctx,teamnum):
-		parser = tbapi.TBAParser('3572', 'Dozer', 'Alpha 0.1')
-		team = parser.get_team('frc' + teamnum)
+	async def tba (self,ctx,task,teamnum):
 		"""Retrieve information about this guild."""
+		parser = tbapi.TBAParser('3572', 'Dozer', 'Alpha 0.1')
+		if task = team
+		team = parser.get_team('frc' + teamnum)
 		guild = ctx.guild
 		e = discord.Embed(color=blurple)
 		e.add_field(name='Team Name', value=team.nickname)
@@ -34,7 +35,8 @@ class Custom(Cog):
 		e.add_field(name='Team Motto', value=team.motto)
 		e.add_field(name='Team Website', value=team.website)
 		e.add_field(name='TBA Page', value='https://www.thebluealliance.com/team/' + teamnum)
-		
 		await ctx.send(embed=e)
+		if task = awards
+		await ctx.send('Awards' + teamnum)
 def setup(bot):
 	bot.add_cog(Custom(bot))
