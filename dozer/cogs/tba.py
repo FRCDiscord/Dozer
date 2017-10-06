5d1824cfde02757d61b5bd546f6fbd1c181d41c1
Michael Cao <mike.x.cao@gmail.com>
2017-10-06 07:56:51 -0400

    Values are no longer hardcoded

--- dozer/cogs/tba.py
+++ dozer/cogs/tba.py
@@ -7,7 +7,7 @@ blurple = discord.Color.blurple()
 class tba(Cog):
 	def __init__(self, bot):
 		super().__init__(bot)
-		self.parser = tbapi.TBAParser('0000', 'Dozer', 'Beta 0.7')
+		self.parser = tbapi.TBAParser(self.config['tba_team'], self.config['tba_application'], self.config['tba_version'])
 	
 	@group(invoke_without_command=False)
 	async def tba(self, ctx):
