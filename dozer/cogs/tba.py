e41f885bed66305a2c1d385532b9cb733a82a3ab
770352 <770352@monashores.net>
2017-10-05 21:42:08 -0400

    Finished team command in TBA
    Special thanks to enderman0

--- dozer/cogs/tba.py
+++ dozer/cogs/tba.py
@@ -5,19 +5,21 @@ from ._utils import *
 blurple = discord.Color.blurple()
 
 class tba(Cog):
-	def _init_(self):
-		parser = tbapi.TBAParser('0000', 'Dozer', 'Beta 0.7')
+	def __init__(self, bot):
+		super().__init__(bot)
+		self.parser = tbapi.TBAParser('0000', 'Dozer', 'Beta 0.7')
 	
-	@group(invoke_without_command=True)
-	async def tba(self, ctx, teamnum):
+	@group(invoke_without_command=False)
+	async def tba(self, ctx):
 		"""Pulls data on FRC teams from The Blue Alliance."""
-		teamdata = parser.get_team('frc' + teamnum)
+		
 		tba.example_usage = """
 	`{prefix}tba team <team-number>` - Pulls information about an FRC team.
-	`{prefix}tba raw <team-number>` - Pulls raw data for an FRC Team.
+	`{prefix}tba traw <team-number>` - Pulls raw data for an FRC Team.
 	"""
 	@tba.command()
 	async def team(self, ctx, teamnum):
+			teamdata = self.parser.get_team('frc' + teamnum)
 			guild = ctx.guild
 			e = discord.Embed(color=blurple)
 			e.add_field(name='Team Name', value=teamdata.nickname)
@@ -30,7 +32,10 @@ class tba(Cog):
 			e.add_field(name='Team Website', value=teamdata.website)
 			e.add_field(name='TBA Page', value='https://www.thebluealliance.com/team/' + teamnum)
 			await ctx.send(embed=e)
-	
-		
+	@tba.command()
+	async def traw(self, ctx, teamnum):
+		 teamdata = self.parser.get_team('frc' + teamnum)
+		 await ctx.send(teamdata.raw)
+		 
 def setup(bot):
 	bot.add_cog(tba(bot))
