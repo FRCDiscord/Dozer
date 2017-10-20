cb86a26877332291b6ba9a35d58909ca369b2c92
LeoPiGuy <leopiguy@gmail.com>
2017-10-20 09:59:23 -0400

    Added a New Member command.
    Complete with a config and database storage, this New Member system in the moderation cog is a full package.
    
    Added Commands: {prefic}nmconfig

--- dozer/cogs/moderation.py
+++ dozer/cogs/moderation.py
@@ -5,6 +5,8 @@ import discord
 
 
 class Moderation(Cog):
+	def __init__(self, bot):
+		super().__init__(bot)
 	@command()
 	@has_permissions(ban_members=True)
 	@bot_has_permissions(ban_members=True)
@@ -74,11 +76,51 @@ class Moderation(Cog):
 				session.add(config)
 			await ctx.send(ctx.message.author.mention + ', modlog settings configured!')
 
+	async def on_message(self, message):
+		if message.author.bot: return
+		if not message.guild.me.guild_permissions.manage_roles: return
+
+		with db.Session() as session:
+			config = session.query(GuildNewMemmber).filter_by(guild_id=message.guild.id).one_or_none()
+			if config is not None:
+				string = config.message
+				content = message.content.casefold()
+				if string not in content: return
+				channel = config.channel_id
+				role_id = config.role_id
+				if message.channel.id != channel: return
+				await message.author.add_roles(discord.utils.get(message.guild.roles, id=role_id))
+
+	@command()
+	#@has_permissions(administrator=True)
+	async def nmconfig(self, ctx, channel_mentions: discord.TextChannel, role_id: discord.Role, *, message):
+		"""Set the config for the new members channel"""
+		with db.Session() as session:
+			config = session.query(GuildNewMemmber).filter_by(guild_id=ctx.guild.id).one_or_none()
+			if config is not None:
+				config.channel_id = channel_mentions.id
+				config.role_id = role_id.id
+				config.message = message.casefold()
+				role_name = role_id.name
+				await ctx.send("New Member Channel configured as: {channel}. Role configured as: {role}. Message: {message}".format(channel=ctx.channel.name, role=role_name, message=message))
+			else:
+				config = GuildNewMemmber(guild_id=ctx.guild.id, channel_id=channel_mentions.id, role_id=role_id.id, message=message.casefold())
+				session.add(config)
+				role_name = role_id.name
+				await ctx.send("New Member Channel configured as: {channel}. Role configured as: {role}. Message: {message}".format(channel=ctx.channel.name, role=role_name, message=message))
+
 class Guildmodlog(db.DatabaseObject):
 	__tablename__ = 'modlogconfig'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String)
 	modlog_channel = db.Column(db.Integer)
+
+class GuildNewMemmber(db.DatabaseObject):
+	__tablename__ = 'new_members'
+	guild_id = db.Column(db.Integer, primary_key=True)
+	channel_id = db.Column(db.Integer)
+	role_id = db.Column(db.Integer)
+	message = db.Column(db.String)
 
 def setup(bot):
 	bot.add_cog(Moderation(bot))
