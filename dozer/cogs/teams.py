0357b7451b4fac9afbfaecd06f4260343be9065e
Travis Weir <twgamefreak@gmail.com>
2017-12-22 00:44:10 -0600

    Made a command for seeing who's on a team

--- dozer/cogs/teams.py
+++ dozer/cogs/teams.py
@@ -1,10 +1,12 @@
 
 from .. import db
 from ._utils import *
-from discord.ext.commands import BadArgument
-
+import discord
+from discord.ext.commands import MemberConverter
 
 # noinspection PyUnboundLocalVariable
+
+
 class Teams(Cog):
 	@command()
 	async def setteam(self, ctx, team_number, team_type):
@@ -23,6 +25,29 @@ class Teams(Cog):
 					dbtransaction = TeamNumbers(user_id=ctx.author.id, frc_team=int(team_number))
 				session.add(dbtransaction)
 		await ctx.send("Team number set!")
+
+	@command()
+	async def onteam(self, ctx, team_number, team_type):
+		with db.Session() as session:
+			badteamtype = False
+			if team_type == 'frc':
+				users = session.query(TeamNumbers).filter_by(frc_team=team_number).all()
+			elif team_type == 'ftc':
+				users = session.query(TeamNumbers).filter_by(ftc_team=team_number).all()
+			else:
+				await ctx.send("Please specify a valid team type!")
+				users = []
+				badteamtype = True
+			if len(users) <= 0:
+				if not badteamtype:
+					await ctx.send("Nobody on that team found!")
+			else:
+				e = discord.Embed(type='rich')
+				e.title = 'Users on team {}'.format(team_number)
+				for i in users:
+					user = ctx.guild.get_member(i.user_id)
+					e.add_field(name=user.display_name, value=user.mention, inline=False)
+				await ctx.send(embed=e)
 
 
 class TeamNumbers(db.DatabaseObject):
