6f461b92e6f5c7f0e22172204c9c76e2847bcaf3
Travis <tweirtx@gmail.com>
2018-01-07 22:44:58 -0600

    Applied changes to even more TBA things

--- dozer/cogs/tba.py
+++ dozer/cogs/tba.py
@@ -60,15 +60,17 @@ class TBA(Cog):
 		Get raw TBA API output for a team.
 		This command is really only useful for development.
 		"""
+		team_data = self.parser.get_team(team_num)
 		try:
-			team_data = self.parser.get_team(team_num)
-		except KeyError:
+			getattr(team_data, "Errors")
+		except tbapi.InvalidKeyError:
+			e = discord.Embed(color=blurple)
+			e.set_author(name='FIRST® Robotics Competition Team {}'.format(team_num), url='https://www.thebluealliance.com/team/{}'.format(team_num), icon_url='http://i.imgur.com/V8nrobr.png')
+			e.add_field(name='Raw Data', value=team_data.flatten())
+			e.set_footer(text='Triggered by ' + ctx.author.display_name)
+			await ctx.send(embed=e)
+		else:
 			raise BadArgument('Team {} does not exist.'.format(team_num))
-		e = discord.Embed(color=blurple)
-		e.set_author(name='FIRST® Robotics Competition Team {}'.format(team_num), url='https://www.thebluealliance.com/team/{}'.format(team_num), icon_url='http://i.imgur.com/V8nrobr.png')
-		e.add_field(name='Raw Data', value=team_data.flatten())
-		e.set_footer(text='Triggered by ' + ctx.author.display_name)
-		await ctx.send(embed=e)
 
 	raw.example_usage = """
 	`{prefix}tba raw 4150` - show raw information on team 4150, FRobotics
@@ -79,39 +81,42 @@ class TBA(Cog):
 		"""
 		Get the timezone of a team based on the team number.
 		"""
+
+		team_data = self.parser.get_team(team_num)
 		try:
-			team_data = self.parser.get_team(team_num)
-		except KeyError:
-			raise BadArgument('Team {} does not exist.'.format(team_num))
-		location = '{0.city}, {0.state_prov} {0.postal_code}, {0.country}'.format(team_data)
-		gmaps = googlemaps.Client(key=self.gmaps_key)
-		geolocator = Nominatim()
-		geolocation = geolocator.geocode(location)
-		timezone = gmaps.timezone(location="{}, {}".format(geolocation.latitude, geolocation.longitude), language="json")
-		utc_offset = int(timezone["rawOffset"])/3600
-		if timezone["dstOffset"] == 3600:
-			utc_offset += 1
-		utc_timedelta = timedelta(hours = utc_offset)
-		currentUTCTime = datetime.datetime.utcnow()
-		currentTime = currentUTCTime+utc_timedelta
-		current_hour = currentTime.hour
-		current_hour_original = current_hour
-		dayTime = "AM"
-		if current_hour > 12:
-			current_hour -= 12
-			dayTime = "PM"
-		elif current_hour == 12:
-			dayTime = "PM"
-		elif current_hour == 0:
-			current_hour = 12
+			getattr(team_data, "Errors")
+		except tbapi.InvalidKeyError:
+			location = '{0.city}, {0.state_prov} {0.postal_code}, {0.country}'.format(team_data)
+			gmaps = googlemaps.Client(key=self.gmaps_key)
+			geolocator = Nominatim()
+			geolocation = geolocator.geocode(location)
+			timezone = gmaps.timezone(location="{}, {}".format(geolocation.latitude, geolocation.longitude), language="json")
+			utc_offset = int(timezone["rawOffset"])/3600
+			if timezone["dstOffset"] == 3600:
+				utc_offset += 1
+			utc_timedelta = timedelta(hours = utc_offset)
+			currentUTCTime = datetime.datetime.utcnow()
+			currentTime = currentUTCTime+utc_timedelta
+			current_hour = currentTime.hour
+			current_hour_original = current_hour
 			dayTime = "AM"
-		current_minute = currentTime.minute
-		if current_minute < 10:
-			current_minute = "0{}".format(current_minute)
-		current_second = currentTime.second
-		if current_second < 10:
-			current_second = "0{}".format(current_second)
-		await ctx.send("Timezone: {0} UTC{1:+g} \nCurrent Time: {2}:{3}:{4} {5} ({6}:{3}:{4})".format(timezone["timeZoneName"], utc_offset, current_hour, current_minute, current_second, dayTime, current_hour_original)) 
+			if current_hour > 12:
+				current_hour -= 12
+				dayTime = "PM"
+			elif current_hour == 12:
+				dayTime = "PM"
+			elif current_hour == 0:
+				current_hour = 12
+				dayTime = "AM"
+			current_minute = currentTime.minute
+			if current_minute < 10:
+				current_minute = "0{}".format(current_minute)
+			current_second = currentTime.second
+			if current_second < 10:
+				current_second = "0{}".format(current_second)
+			await ctx.send("Timezone: {0} UTC{1:+g} \nCurrent Time: {2}:{3}:{4} {5} ({6}:{3}:{4})".format(timezone["timeZoneName"], utc_offset, current_hour, current_minute, current_second, dayTime, current_hour_original))
+		else:
+			raise BadArgument('Team {} does not exist.'.format(team_num))
 					
 	timezone.example_usage = """
 	`{prefix}timezone 3572` - show the local time of team 3572, Wavelength
