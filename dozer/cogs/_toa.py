5277206fb6ff1a7500a5ebab3448b9042b181b8e
Ofek Ashery <ofek.ashery@gmail.com>
2018-10-19 22:33:42 +0300

    Update TOA

--- dozer/cogs/_toa.py
+++ dozer/cogs/_toa.py
@@ -14,15 +14,16 @@ class TOAParser:
     A class to make async requests to The Orange Alliance.
     """
 
-    def __init__(self, api_key, aiohttp_session, base_url="https://theorangealliance.org/apiv2/", app_name="Dozer",
+    def __init__(self, api_key, aiohttp_session, base_url="https://theorangealliance.org/api/", app_name="Dozer",
                  ratelimit=True):
         self.last_req = datetime.now()
         self.ratelimit = ratelimit
         self.base = base_url
         self.http = aiohttp_session
         self.headers = {
-            "X-Application-Origin": app_name,
-            "X-TOA-Key": api_key
+            'X-Application-Origin': app_name,
+            'X-TOA-Key': api_key,
+            'Content-Type': 'application/json'
         }
 
     async def req(self, endpoint):
@@ -39,24 +40,8 @@ class TOAParser:
             try:
                 async with async_timeout.timeout(5) as _, self.http.get(urljoin(self.base, endpoint),
                                                                         headers=self.headers) as response:
-                    res = TOAResponse()
-                    # it seems sometimes toa forgets to return data as application/json and not text/html
-                    data = json.loads(await response.text())
-                    if data:
-                        res._update(data[0])
-                    else:
-                        res.error = True
-                    return res
+                    return await response.text()
             except aiohttp.ClientError:
                 tries += 1
                 if tries > 3:
                     raise
-
-
-class TOAResponse:
-    """Represents a response from the TOA API."""
-    def __init__(self):
-        self.error = False
-
-    def _update(self, v):
-        self.__dict__.update(v)
