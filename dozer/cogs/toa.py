1755970b7811582055d91f3b767aaea53134413a
Travis Weir <tweirtx@gmail.com>
2018-11-19 11:52:08 -0600

    Folded _toa into toa cog. Also kept pickle because ng uses it

--- dozer/cogs/toa.py
+++ dozer/cogs/toa.py
@@ -1,13 +1,53 @@
 """Provides commands that pull information from The Orange Alliance, an FTC info API."""
-import gzip
-import pickle
 
+from asyncio import sleep
+from datetime import datetime
+from urllib.parse import urljoin
+import json
+import aiohttp
+import async_timeout
 import discord
-
-from ._toa import *
 from ._utils import *
 
 embed_color = discord.Color(0xff9800)
+
+
+class TOAParser:
+    """
+    A class to make async requests to The Orange Alliance.
+    """
+
+    def __init__(self, api_key, aiohttp_session, base_url="https://theorangealliance.org/api/", app_name="Dozer",
+                 ratelimit=True):
+        self.last_req = datetime.now()
+        self.ratelimit = ratelimit
+        self.base = base_url
+        self.http = aiohttp_session
+        self.headers = {
+            'X-Application-Origin': app_name,
+            'X-TOA-Key': api_key,
+            'Content-Type': 'application/json'
+        }
+
+    async def req(self, endpoint):
+        """Make an async request at the specified endpoint, waiting to let the ratelimit cool off."""
+        if self.ratelimit:
+            # this will delay a request to avoid the ratelimit
+            now = datetime.now()
+            diff = (now - self.last_req).total_seconds()
+            self.last_req = now
+            if diff < 2.2:  # have a 200 ms fudge factor
+                await sleep(2.2 - diff)
+        tries = 0
+        while True:
+            try:
+                async with async_timeout.timeout(5) as _, self.http.get(urljoin(self.base, endpoint),
+                                                                        headers=self.headers) as response:
+                    return await response.text()
+            except aiohttp.ClientError:
+                tries += 1
+                if tries > 3:
+                    raise
 
 
 class TOA(Cog):
@@ -16,8 +56,6 @@ class TOA(Cog):
         super().__init__(bot)
         self.parser = TOAParser(bot.config['toa']['key'], bot.http._session, app_name=bot.config['toa']['app_name'])
         # The line above has an error (bot.http._session is a protected class)
-        with gzip.open("ftc_teams.pickle.gz") as f:
-            self._teams = pickle.load(f)
 
     @group(invoke_without_command=True)
     async def toa(self, ctx, team_num: int):
