d967830623ef487858f4271fc903cf322c07d687
Guinea Wheek <guineawheek@gmail.com>
2019-01-16 00:52:22 -0500

    add docstrings to ConfigCache

--- dozer/db.py
+++ dozer/db.py
@@ -28,26 +28,30 @@ class CtxSession(Session):
         return self.__exit__(err_type, err, tb)
 
 class ConfigCache:
+    """Class that will reduce calls to sqlalchemy as much as possible. Has no growth limit (yet)"""
     def __init__(self, table):
         self.cache = {}
         self.table = table
 
     @staticmethod
     def _hash_dict(dic):
-        # sort the keys to make this repeatable
+        """Makes a dict hashable by turning it into a tuple of tuples"""
         values = []
+        # sort the keys to make this repeatable; this allows consistency even when insertion order is different
         for k in sorted(dic):
             values.append((k, dic[k]))
         return tuple(values)
 
     def query(self, **kwargs):
+        """Query the cache for an entry matching the kwargs, then try again using the database."""
         query_hash = self._hash_dict(kwargs)
-        if not query_hash in self.cache:
+        if query_hash not in self.cache:
             with Session() as session:
                 self.cache[query_hash] = session.query(self.table).filter_by(**kwargs).one_or_none()
         return self.cache[query_hash]
 
     def invalidate_entry(self, **kwargs):
+        """Removes an entry from the cache if it exists - used to mark changed data."""
         query_hash = self._hash_dict(kwargs)
         if query_hash in self.cache:
             del self.cache[query_hash]
