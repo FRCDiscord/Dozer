783662f480c37473857719b341b4830538172b15
Aidan <aidanfromprogramming@gmail.com>
2021-03-15 17:34:42 -0400

    Added first version of new fight
    Signed-off-by: Aidan <aidanfromprogramming@gmail.com>

--- dozer/cogs/fun.py
+++ dozer/cogs/fun.py
@@ -1,18 +1,21 @@
 """Adds fun commands to the bot"""
+import asyncio
 import random
 from asyncio import sleep
 
 import discord
-from discord.ext.commands import cooldown, BucketType, guild_only
+from discord.ext.commands import cooldown, BucketType, guild_only, BadArgument, MissingPermissions
+
 from ._utils import *
 
 
 class Fun(Cog):
     """Fun commands"""
-    @guild_only()
-    @cooldown(1, 20, BucketType.channel)
-    @command()
-    async def fight(self, ctx, opponent: discord.Member):
+
+    def __init__(self, bot):
+        super().__init__(bot)
+
+    async def battle(self, ctx, opponent: discord.Member, delete_result=True):
         """Start a fight with another user."""
         responses = [
             "was hit on the head by",
@@ -40,7 +43,7 @@ class Fun(Cog):
 
         damages = [100, 150, 200, 300, 50, 250, 420]
         players = [ctx.author, opponent]
-        hps = [1000, 1000]
+        hps = [1400, 1400]
         turn = random.randint(0, 1)
 
         messages = []
@@ -60,9 +63,11 @@ class Fun(Cog):
                 )))
             await sleep(1.5)
             turn = opp_idx
-        await ctx.send(
+        win_msg = await ctx.send(
             "{loser} lost! GG {winner}!".format(loser=players[turn].mention, winner=players[(turn + 1) % 2].mention))
         await sleep(5)
+        if delete_result:
+            await win_msg.delete()
         # bulk delete if we have the manage messages permission
         if ctx.channel.permissions_for(ctx.me).manage_messages:
             await ctx.channel.delete_messages(messages)
@@ -71,9 +76,74 @@ class Fun(Cog):
             for msg in messages:
                 await msg.delete()
 
+        return players[turn], players[(turn + 1) % 2]
+
+    @guild_only()
+    @discord.ext.commands.cooldown(1, 5, BucketType.channel)
+    @discord.ext.commands.max_concurrency(1, wait=False)
+    @command()
+    async def fight(self, ctx, opponent: discord.Member, wager: int = 0):
+        """Battles or something"""
+
+        levels = self.bot.get_cog("Levels")
+
+        if wager == 0:
+            await self.battle(ctx, opponent, delete_result=False)
+            return
+
+        if not levels.guild_settings.get(ctx.guild.id).enabled:
+            raise BadArgument("Levels must be enabled to fight with xp wagers")
+
+        if wager > 1000:
+            raise BadArgument("Wagers cannot be higher than 1000")
+
+        if wager < 0:
+            raise BadArgument("Wagers cannot be a negative amount")
+
+        author_levels = await levels.load_member(ctx.guild.id, ctx.author.id)
+        opponent_levels = await levels.load_member(ctx.guild.id, opponent.id)
+
+        if author_levels.total_xp < wager:
+            raise BadArgument(f"You not have enough XP to fulfill the wager")
+
+        if opponent_levels.total_xp < wager:
+            raise BadArgument(f"{opponent} does not have enough XP to fulfill the wager")
+
+        embed = discord.Embed(description=f"{ctx.author.mention} has challenged {opponent.mention} to a fight with a wager of"
+                                          f" {wager}xp")
+        embed.set_footer(text=f"{opponent.display_name} react to the ✅ to agree to the fight")
+        embed.set_author(name=f"{ctx.author.display_name} vs {opponent.display_name}")
+
+        msg = await ctx.send(embed=embed)
+        try:
+            await msg.add_reaction("✅")
+        except discord.Forbidden:
+            raise MissingPermissions(f"**{ctx.bot.user}** does not have the permission to add reacts")
+        try:
+            await self.bot.wait_for('reaction_add', timeout=60, check=lambda reaction, reactor:
+            reaction.emoji == "✅" and reactor == opponent and reaction.message == msg)
+            embed.set_footer(text="")
+            looser, winner = await self.battle(ctx, opponent)
+            embed.colour = 0x00ff00
+            embed.add_field(name="Results", value=f"{winner.mention} beat {looser.mention}"
+                                                  f"\n{ctx.author.mention} now is at "
+                                                  f"level {levels.level_for_total_xp(author_levels.total_xp)} ({author_levels.total_xp} XP) "
+                                                  f"\n{opponent.mention} now is at "
+                                                  f"level {levels.level_for_total_xp(opponent_levels.total_xp)} ({opponent_levels.total_xp} XP)")
+
+            author_levels.total_xp += wager if winner is ctx.author else -wager
+            opponent_levels.total_xp += wager if winner is opponent else -wager
+
+        except asyncio.TimeoutError:
+            await msg.clear_reactions()
+            embed.add_field(name="Results", value=f"{opponent.mention} failed to accept in time, fight canceled")
+            embed.colour = 0xff0000
+
+        await msg.edit(content=None, embed=embed)
+
     fight.example_usage = """
-    `{prefix}fight @user2#2322 - Initiates a fight with @user2#2322`
-    """
+        `{prefix}fight @user2#2322 - Initiates a fight with @user2#2322`
+        """
 
 
 def setup(bot):
