9dcfcd690b44fffcbc41766992c9faca52628e07
Ben <bencurby80@gmail.com>
2022-04-12 13:55:51 -0400

    fixed pylint

--- dozer/cogs/roles.py
+++ dozer/cogs/roles.py
@@ -130,12 +130,11 @@ class Roles(Cog):
     @Cog.listener('on_guild_role_update')
     async def on_role_edit(self, old, new):
         """Changes role names in database when they are changed in the guild"""
-        if self.normalize(old.name)!=self.normalize(new.name):
+        if self.normalize(old.name) != self.normalize(new.name):
             results = await GiveableRole.get_by(norm_name=self.normalize(old.name), guild_id=old.guild.id)
             if results:
                 DOZER_LOGGER.debug(f"Role {new.id} name updated. updating name")
                 await GiveableRole.from_role(new).update_or_add()
-    
 
     @Cog.listener('on_guild_role_delete')
     async def on_role_delete(self, old):
