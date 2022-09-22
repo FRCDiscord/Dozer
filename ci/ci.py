e25e38d0b8a7a91eedc310e501128b9323f982d5
Ava <transorsmth@gmail.com>
2022-09-22 10:59:37 -0400

    Change tabs in ci.py for spaces.

--- ci/ci.py
+++ ci/ci.py
@@ -1,9 +1,8 @@
-
 with open('./ci/cilog.txt', 'r') as log:
-	lines = log.readlines()
-	line_num = len(lines) - 2
-	line = lines[line_num]
-	if "10.00" in line:
-		exit(0)
-	else:
-		exit(1)
+    lines = log.readlines()
+    line_num = len(lines) - 2
+    line = lines[line_num]
+    if "10.00" in line:
+        exit(0)
+    else:
+        exit(1)
