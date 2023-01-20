268224d36eb619620c9a54fd51c4362a17cf3183
Ava <transorsmth@gmail.com>
2023-01-20 10:34:52 -0500

    I was stupid, I fixed it

--- dozer/cogs/levels.py
+++ dozer/cogs/levels.py
@@ -704,13 +704,10 @@ class Levels(Cog):
                 rank = db_record.get("rank")
             else:
                 rank = count
-            await ctx.send(f"Progress: {str((total_xp - level_floor) / (level_xp - level_floor))}")
-            await ctx.send(str(((total_xp - level_floor) / (level_xp - level_floor))*200))
-            await ctx.send(f"{total_xp}, {level_xp}")
             draw.text((100, 42), f'Level {level}, {total_xp - level_floor}/{level_xp} XP to level up. Level {level}.  ')
             draw.text((100, 55), f'#{rank} of {count} in this server')
 
-            new_bar(100, 73, 200, 12, (total_xp - level_floor) / (level_xp - level_floor))
+            new_bar(100, 73, 200, 12, (total_xp - level_floor) / (level_xp))
 
             embed.description = (f"Level {level}, {total_xp - level_floor}/{level_xp} XP to level up ({total_xp} total)\n"
                                  f"#{rank} of {count} in this server")
