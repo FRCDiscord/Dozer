9eb7587c6b8a61d71ef1f3bd20a692802a66f1b5
OmegaMetor <42892531+OmegaMetor@users.noreply.github.com>
2023-04-16 22:13:33 -0500

    Put the stuff in the right spot

--- dozer/cogs/general.py
+++ dozer/cogs/general.py
@@ -21,7 +21,7 @@ class General(Cog):
         if ctx.guild is None:
             location = 'DMs'
         else:
-            location = f'the **{None, text=clean(ctx.guild.name)}** server'
+            location = f'the **{text=clean(None, ctx.guild.name)}** server'
         response = await ctx.send(f'Pong! We\'re in {location}.')
         delay = response.created_at - ctx.message.created_at
         await response.edit(
