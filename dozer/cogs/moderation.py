444e0727c7ad2a29e00d2b32b1b9938781e2b13b
travis weir <tweirtx@gmail.com>
2024-03-31 23:33:41 -0500

    Fix requireteam because it hasn't worked for 2 years lol

--- dozer/cogs/moderation.py
+++ dozer/cogs/moderation.py
@@ -887,7 +887,7 @@ class Moderation(Cog):
     @command()
     @has_permissions(administrator=True)
     async def nmconfig(self, ctx: DozerContext, channel_mention: discord.TextChannel, role: discord.Role, *, message,
-                       requireteam=None):
+                       requireteam: bool = None):
         """Sets the config for the new members channel"""
         config = await GuildNewMember.get_by(guild_id=ctx.guild.id)
         if len(config) != 0:
